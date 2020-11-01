73058fb95e362cd49781f6cf2569e7c339075245
Humayun Javed <arnetraff@gmail.com>
2020-11-01 19:32:49 +0500

    Anims

--- src/components/PhotoGrid/Photo.js
+++ src/components/PhotoGrid/Photo.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 
 import styles from "./Photo.module.css";
 
 
 
 export default function Photo(props) {
+  let [pos,setPos] = useState(false);
+  // console.log(styles);
   return (
-    <div className = {styles.photo}>
-        <img className = {styles.img } src={props.photoSrc} />
+    <div className = {styles.photo} onClick={()=>setPos(true)}>
+        <img className = {`${styles.img} ${ pos? styles.full : ''}`} src={props.photoSrc} />
     </div>
   )
 }
