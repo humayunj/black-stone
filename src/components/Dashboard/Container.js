14ef20ee8c6f26b45c23bcf6227ed78ebf71f597
Mian Husnain <mlhlk1212@gmail.com>
2020-11-02 00:58:29 +0500

    add about.js

--- src/components/Dashboard/Container.js
+++ src/components/Dashboard/Container.js
@@ -1,12 +1,30 @@
-import React from "react";
-import TopNav from "./TopNav";
+import React, { useState } from "react";
 import PhotoGrid from "../PhotoGrid/Grid";
+import TopNav from "./TopNav";
 
 export default function DashboardContainer(props) {
+  const [defClass,setdefClass] = useState({
+    'display':'none',
+    'maxHeight':'0vh',
+  })
+    
   return (
     <div styles="1000px">
-      <TopNav/>
-      <PhotoGrid/>
+      <TopNav setAbout={setdefClass}/>
+      <div>
+          <PhotoGrid/>
+      </div>
+      <div style = {{display:defClass.display,
+      zIndex:2,
+      position:"fixed",
+      top:'0',
+      left:'0',
+      backgroundColor:"black",
+      width:'100vw',height:defClass.height,
+      maxHeight:defClass.maxHeight}}>
+        {console.log("about")}
+        <h1>usanins</h1>
+      </div>
     </div>
   );
 } 
--- src/components/Dashboard/TopNav.js
+++ src/components/Dashboard/TopNav.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import styles from "./TopNav.module.css";
 
-export default function TopNav(props) {
+export default function TopNav({setAbout}) {
   return (
     <div class={styles.nav}>
       <div className={`${styles.item} ${styles.brand}`}>black stone</div>
-      <div className={`${styles.item} ${styles.about}`}>about.</div>
+      <div className={`${styles.item} ${styles.about}`} onClick={() =>setAbout({
+           'display':'block',
+           'maxHeight':'100vh',
+           'height':'100vh',
+      })}>about.</div>
     </div>
   );
 }
